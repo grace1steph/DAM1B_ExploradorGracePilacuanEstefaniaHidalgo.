5389cc7ff7de473be7f0b5079f197d3aca272e5c
grace1steph <gpilacuhid1@educacion.navarra.es>
2025-01-07 23:25:22 +0100

    He realizado la clase Explorador, con sus atributos y los métodos

--- src/Explorador.java
+++ src/Explorador.java
@@ -1,8 +1,58 @@
 public class Explorador {
+    private final int ARRIBA = 1;
+    private final int ABAJO = 2;
+    private final int IZQUIERDA = 3;
+    private final int DERECHA =4;
+    private String nombre;
+    private Posicion posicionActual;
+
     public Explorador(String nombre) {
         this.nombre = nombre;
-        this.posicionColumna = 0;
-        int fila = 0;
-        
+        int fila = (int) (Math.random() *6);
+        this.posicionActual = new Posicion(fila,0);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Posicion getPosicionActual() {
+        return posicionActual;
+    }
+
+    public void setPosicionActual(Posicion posicionActual) {
+        this.posicionActual = posicionActual;
+    }
+
+    public void moverse (int direccion){
+        if (direccion >= 1 && direccion <= 4 ){
+            if(direccion == ARRIBA){
+                if (posicionActual.getCoordenadaFila() > 0 ){
+                    posicionActual.setCoordenadaFila(posicionActual.getCoordenadaFila() -1 );
+                }else {
+                    System.out.println("No puedes salir del mapa");
+                }
+            } else if (direccion == ABAJO){
+                if (posicionActual.getCoordenadaFila() < 5){
+                    posicionActual.setCoordenadaFila(posicionActual.getCoordenadaFila() +1 );
+                }else {
+                    System.out.println("No puedes salir del mapa");
+                }
+            } else if (direccion == IZQUIERDA) {
+                if (posicionActual.getCoordenadaCol() < 19) {
+                    posicionActual.setCoordenadaCol(posicionActual.getCoordenadaCol() - 1);
+                } else {
+                    System.out.println("No puedes salir del mapa");
+                }
+            } else if (direccion == DERECHA) {
+                if (posicionActual.getCoordenadaCol() > 0){
+                    posicionActual.setCoordenadaCol(posicionActual.getCoordenadaCol() + 1);
+                } else {
+                    System.out.println("No puedes salir del mapa");
+                }
+            }
+        } else {
+            System.out.println("La dirección es errónea");
+        }
     }
 }
